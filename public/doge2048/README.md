0217c22c8bc436c4cdf74614dbf9b0b88cb8f2d1
Steve Gehrman <sgehrman@gmail.com>
2021-11-07 17:16:50 -0800

    fix

---
+++ public/doge2048/README.md
@@ -0,0 +1,3 @@
+# doge2048
+
+2048 with DOGE
--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use("/", express.static(__dirname + "/../public/doge2048"));
+
 // init server
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
-});
-
-app.get("/", async (req, res) => {
-  res.send('hi');
 });
