b1c98b2533b9b39a3a1ddf72e3634785760da217
Steve Gehrman <sgehrman@gmail.com>
2021-11-18 22:28:02 -0800

    fixe

--- doge2048/src/components/UnlockedColumn/index.jsx
+++ doge2048/src/components/UnlockedColumn/index.jsx
@@ -3,9 +3,21 @@ import styles from './UnlockedColumn.module.scss';
 export const UnlockedColumn = () => {
   return (
     <div className={styles.main}>
-      <div>test</div>
-      <div>ur</div>
-      <div>skilz</div>
+      <div className={styles.item}>
+        <div>1024</div>
+      </div>
+
+      <div className={styles.item}>
+        <div>2048</div>
+      </div>
+
+      <div className={styles.item}>
+        <div>4096</div>
+      </div>
+
+      <div className={styles.item}>
+        <div>8192</div>
+      </div>
     </div>
   );
 };
