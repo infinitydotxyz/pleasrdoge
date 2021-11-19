29bc8c5aee9fade1e18d857fc0b6b90540edf04c
Steve Gehrman <sgehrman@gmail.com>
2021-11-18 21:33:31 -0800

    refact

--- doge2048/src/App.js
+++ doge2048/src/App.js
@@ -1,13 +1,14 @@
-import "./App.css";
-import "./js/game_manager.js";
-import "./js/animframe_polyfill.js";
-import "./js/keyboard_input_manager.js";
-import "./js/html_actuator.js";
-import "./js/grid.js";
-import "./js/tile.js";
-import "./js/local_score_manager.js";
-import "./js/application.js";
-import "./style/main.css";
+import './App.css';
+import './js/game_manager.js';
+import './js/animframe_polyfill.js';
+import './js/keyboard_input_manager.js';
+import './js/html_actuator.js';
+import './js/grid.js';
+import './js/tile.js';
+import './js/local_score_manager.js';
+import './js/application.js';
+import './style/main.css';
+import { GameGrid } from './components/GameGrid/GameGrid';
 
 function App() {
   return (
@@ -15,10 +16,10 @@ function App() {
       <div className="container">
         <div className="heading">
           <h1 className="title">
-            DOGE<span style={{ color: "red" }}>2</span>
-            <span style={{ color: "#00FF00" }}>0</span>
-            <span style={{ color: "blue" }}>4</span>
-            <span style={{ color: "#00FFFF" }}>8</span>
+            DOGE<span style={{ color: 'red' }}>2</span>
+            <span style={{ color: '#00FF00' }}>0</span>
+            <span style={{ color: 'blue' }}>4</span>
+            <span style={{ color: '#00FFFF' }}>8</span>
           </h1>
           <div className="scores-container">
             <div className="score-container">0</div>
@@ -33,19 +34,19 @@ function App() {
           <div className="info">
             <p>
               Use your arrow keys or swipe to combine similar Doges and score
-              points!{" "}
+              points!{' '}
             </p>
             <p>Unlock all 11 doges to win!</p>
             <p>
-              {" "}
-              Doge-ified by{" "}
+              {' '}
+              Doge-ified by{' '}
               <a href="http://tinymammals.com" target="_blank" rel="noreferrer">
                 Tiny Mammals.
               </a>
             </p>
             <p>
-              {" "}
-              2048 Created by{" "}
+              {' '}
+              2048 Created by{' '}
               <a
                 href="http://gabrielecirulli.com"
                 target="_blank"
@@ -55,15 +56,15 @@ function App() {
               </a>
             </p>
             <p>
-              Based on{" "}
+              Based on{' '}
               <a
                 href="https://itunes.apple.com/us/app/1024!/id823499224"
                 target="_blank"
                 rel="noreferrer"
               >
                 1024 by Veewo Studio
-              </a>
-              and conceptually similar to{" "}
+              </a>{' '}
+              and conceptually similar to{' '}
               <a
                 href="http://asherv.com/threes/"
                 target="_blank"
@@ -81,32 +82,7 @@ function App() {
             </div>
           </div>
 
-          <div className="grid-container">
-            <div className="grid-row">
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-            </div>
-            <div className="grid-row">
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-            </div>
-            <div className="grid-row">
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-            </div>
-            <div className="grid-row">
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-              <div className="grid-cell"></div>
-            </div>
-          </div>
+          <GameGrid />
 
           <div className="tile-container"></div>
         </div>
---
+++ doge2048/src/components/GameGrid/GameGrid.jsx
@@ -0,0 +1,34 @@
+import styles from './GameGrid.module.scss';
+
+export const GameGrid = () => {
+  return (
+    <div className={styles.gridRow}>
+      <div className="grid-container">
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+          <div className="grid-cell"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
