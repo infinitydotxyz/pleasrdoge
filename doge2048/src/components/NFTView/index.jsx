011a0f80793ac9265a92f99fb11bde8667ef3a01
Steve Gehrman <sgehrman@gmail.com>
2021-11-24 21:42:34 -0800

    fix

--- doge2048/src/components/NFTView/index.jsx
+++ doge2048/src/components/NFTView/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import dogeShakeSpace2 from '../../img/114/doge-shake-space-114.gif';
 
 export const NFTView = () => {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <div
+        style={{ backgroundImage: `url(${dogeShakeSpace2})` }}
+        className={styles.background}
+      ></div>
+    </div>
+  );
 };
