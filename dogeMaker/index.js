6ba7d6810e0500a3f15ddd7f83ddaea7952cd75a
Steve Gehrman <sgehrman@gmail.com>
2021-11-29 07:38:38 -0800

    fix

--- dogeMaker/index.js
+++ dogeMaker/index.js
@@ -1,27 +1,76 @@
 import { createWriteStream, readdirSync, mkdirSync } from 'fs';
 import pkg from 'canvas';
-import sizeOf from 'image-size';
-
 const { createCanvas, loadImage } = pkg;
 
-const saveFile = async ({ outPath, eyeImage, hatImage, glassesImage }) => {
-  try {
-    const source = './images/doge.png';
-    const canvas = createCanvas(800, 800);
-    const ctx = canvas.getContext('2d');
+const imgCache = {};
 
-    let image = await loadImage(source);
-    ctx.drawImage(image, 0, 0);
+const loadImg = async (path) => {
+  let result = imgCache[path];
+
+  if (!result) {
+    result = await loadImage(path);
+
+    imgCache[path] = result;
+  }
+
+  return result;
+};
+
+let cnt = 0;
+
+const saveFile = async (props) => {
+  const {
+    dogeImage,
+    outputPath,
+    starImage,
+    heartImage,
+    hatImage,
+    glassesImage,
+    bowTieImage,
+  } = props;
+
+  if (glassesImage) {
+    if (starImage || heartImage) {
+      return;
+    }
+  } else {
+    if (starImage && heartImage) {
+      return;
+    }
+  }
+
+  cnt++;
+
+  try {
+    let image;
+    let canvas = createCanvas(800, 800);
+    let ctx = canvas.getContext('2d');
+
+    ctx.drawImage(dogeImage, 0, 0);
 
     // Stars on eyes
-    if (eyeImage) {
+    if (starImage) {
       const size = 120;
       const lx = 240;
       const ly = 220;
       const rx = 375;
       const ry = 250;
 
-      image = await loadImage(eyeImage);
+      image = await loadImg(starImage);
+      ctx.drawImage(image, lx, ly, size, size);
+
+      ctx.drawImage(image, rx, ry, size, size);
+    }
+
+    // Stars on eyes
+    if (heartImage) {
+      const size = 120;
+      const lx = 240;
+      const ly = 220;
+      const rx = 375;
+      const ry = 250;
+
+      image = await loadImg(heartImage);
       ctx.drawImage(image, lx, ly, size, size);
 
       ctx.drawImage(image, rx, ry, size, size);
@@ -29,69 +78,128 @@ const saveFile = async ({ outPath, eyeImage, hatImage, glassesImage }) => {
 
     // Hats
     if (hatImage) {
-      const size = 400;
-      const x = 220;
-      const y = 20;
+      const scale = 0.82;
+      const x = 130;
+      const y = 10;
 
-      image = await loadImage(hatImage);
-      ctx.drawImage(image, x, y, size, size);
+      image = await loadImg(hatImage);
+      ctx.drawImage(image, x, y, image.width * scale, image.height * scale);
     }
 
     // Glasses
     if (glassesImage) {
-      const dimensions = sizeOf(glassesImage);
-
       const scale = 0.82;
       const x = 220;
-      const y = 300 - dimensions.height;
+      const y = 300;
 
-      image = await loadImage(glassesImage);
-      ctx.drawImage(
-        image,
-        x,
-        y,
-        dimensions.width * scale,
-        dimensions.height * scale
-      );
+      image = await loadImg(glassesImage);
+
+      ctx.drawImage(image, x, y, image.width * scale, image.height * scale);
     }
 
-    canvas.createPNGStream().pipe(createWriteStream(outPath));
+    // Bow Ties
+    if (bowTieImage) {
+      const scale = 0.32;
+      const x = 220;
+      const y = 500;
+
+      image = await loadImg(bowTieImage);
+      ctx.drawImage(image, x, y, image.width * scale, image.height * scale);
+    }
+
+    canvas.createPNGStream().pipe(createWriteStream(outputPath));
+
+    canvas = null;
+    image = null;
+    ctx = null;
   } catch (err) {
     console.log(err);
     process.exit(1);
   }
 };
 
-const filesInDir = (path) => {
-  const list = readdirSync(path, { withFileTypes: true });
+const filesInDir = (path, listCode) => {
+  let list = readdirSync(path, { withFileTypes: true });
 
-  return list.filter((entry) => {
-    entry.path = `${path}/${entry.name}`;
+  list = list.filter((entry) => {
     return entry.isFile;
   });
+
+  list.forEach((entry, index) => {
+    entry.path = `${path}/${entry.name}`;
+    entry.id = `${listCode}${index}`;
+  });
+
+  return list;
 };
 
-const main = () => {
+const pathForEntries = (entries) => {
+  let result = '';
+
+  for (const e of entries) {
+    if (e.id) {
+      result = `${result}_${e.id}`;
+    }
+  }
+
+  return result;
+};
+
+const main = async () => {
   mkdirSync('./output', { recursive: true });
 
-  const stars = filesInDir('./images/Stars');
-  // const hats = filesInDir('./images/Hats');
-  const glasses = filesInDir('./images/Glasses');
+  const stars = filesInDir('./images/Stars', 's');
+  const hats = filesInDir('./images/Hats', 'h');
+  const glasses = filesInDir('./images/Glasses', 'g');
+  const bowTies = filesInDir('./images/Bow ties', 'b');
+  const hearts = filesInDir('./images/Hearts', 'r');
+
+  // push blank on each list so we can have items without any hat/glasses/stars
+  stars.push({});
+  hats.push({});
+  glasses.push({});
+  bowTies.push({});
+  hearts.push({});
+
+  let max = 100000;
+
+  const dogeImage = await loadImg('./images/doge.png');
 
   // files object contains all files names
   // log them on console
-  // hats.forEach((hatEntry) => {
-  stars.forEach((entry) => {
-    glasses.forEach((glassesEntry) => {
-      saveFile({
-        outPath: `./output/${entry.name + glassesEntry.name}`,
-        eyeImage: entry.path,
-        // hatImage: hatEntry.path,
-        glassesImage: glassesEntry.path,
-      });
-    });
-  });
-  // });
+  for (const hatEntry of hats) {
+    for (const starEntry of stars) {
+      for (const glassesEntry of glasses) {
+        console.log(cnt);
+
+        for (const tiesEntry of bowTies) {
+          for (const heartEntry of hearts) {
+            const path = pathForEntries([
+              starEntry,
+              glassesEntry,
+              hatEntry,
+              tiesEntry,
+              heartEntry,
+            ]);
+
+            if (--max > 0) {
+              await saveFile({
+                dogeImage: dogeImage,
+                outputPath: `./output/${path}.png`,
+                starImage: starEntry.path,
+                heartImage: heartEntry.path,
+                hatImage: hatEntry.path,
+                glassesImage: glassesEntry.path,
+                bowTieImage: tiesEntry.path,
+              });
+            }
+          }
+        }
+      }
+    }
+  }
+
+  console.log(`Total Images: ${cnt}`);
 };
 
 main();
